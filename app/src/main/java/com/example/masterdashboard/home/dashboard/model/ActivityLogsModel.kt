1264a2cbb87c270e88f3135b1746058683e1a24d
Pavandew <pavandewangan4@gmail.com>
2026-05-17 11:08:05 +0530

    update alignemnts

---
+++ app/src/main/java/com/example/masterdashboard/home/dashboard/model/ActivityLogsModel.kt
@@ -0,0 +1,10 @@
+package com.example.masterdashboard.home.dashboard.model
+
+data class ActivityLogsModel(
+
+    val id: String = "",
+    val type: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val time: Long = 0
+)
---
+++ app/src/main/java/com/example/masterdashboard/home/dashboard/model/QuickActionModel.kt
@@ -0,0 +1,7 @@
+package com.example.masterdashboard.home.dashboard.model
+
+data class QuickActionModel(
+    val title: String,
+    val icon: Int,
+    val bgColor: Int
+)
