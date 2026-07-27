c23abd3c6726bdd1d097671f871f1da7b446b3f9
Pavandew <pavandewangan4@gmail.com>
2026-07-28 00:18:58 +0530

    updated some items

--- app/src/main/java/com/example/masterdashboard/staff_dash/waiter_screens/table/models/OrderUiState.kt
+++ app/src/main/java/com/example/masterdashboard/staff_dash/waiter_screens/table/models/OrderUiState.kt
@@ -4,7 +4,11 @@ data class OrderUiState(
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
     val categories: List<MenuCategoryData> = emptyList(),
+    val dietFilters: List<MenuCategoryData> = emptyList(), // Reusing MenuCategoryData for Diet Chips
+    val activeFilterId: String = "ALL_ITEMS",           // Controls which items are FILTERED (All vs Specific)
+    val activeDietFilter: String = "ALL",               // "ALL", "VEG", "NON-VEG"
     val menuItems: List<FoodItemData> = emptyList(),     // Displays the FILTERED items on the menu screen
+    val displayItems: List<MenuItemType> = emptyList(),  // List with headers for the UI
     val allMenuItems: List<FoodItemData> = emptyList(),  // FIX: Absolute master list tracking ALL quantities
     val cartSummary: CartSummaryState = CartSummaryState(0, 0)
 )
